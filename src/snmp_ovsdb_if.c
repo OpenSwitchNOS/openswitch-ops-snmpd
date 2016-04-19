d40cd75133a3cb252e324f18370e5f8e075700d0
Pooja Kulkarni <pooja.kulkarni@hpe.com>
2016-04-19 05:38:59 -0700

    chg: dev: Added addition support for snmpd.conf generation
    snmp_vty.c : fixed defect 2247
    
    Change-Id: Iecd19f4d18545d7464b0034c73a4a3dd9830c80a
    Signed-off-by: Pooja Kulkarni <pooja.kulkarni@hpe.com>

--- src/snmp_ovsdb_if.c
+++ src/snmp_ovsdb_if.c
@@ -41,6 +41,195 @@ ops_snmpd_system_is_configured(void)
 }/* ops_system_is_configured */
 
 static void
+restart_snmpd()
+{
+    int ret = system("systemctl restart snmpd");
+
+    if(ret < 0)
+    {
+        VLOG_ERR("failed to restart snmpd");
+    }
+
+    return;
+}
+
+static void
+generate_snmpd_conf(const struct ovsrec_system* system_row,
+                    const struct ovsrec_snmpv3_user *v3_user)
+{
+    char snmp_engine_id[SNMP_ENGINEID_STR_LEN] = {0};
+    FILE* fp = fopen("/etc/snmp/snmpd.conf","w");
+
+    if(fp == NULL)
+    {
+        VLOG_ERR("Couldn't open snmpd.conf");
+        return;
+    }
+
+    fprintf(fp,"############################################################\n");
+    fprintf(fp,"#\n");
+    fprintf(fp,"#  AGENT BEHAVIOUR\n");
+    fprintf(fp,"#\n\n\n");
+
+    const char* agent_port = smap_get(&system_row->other_config, "snmp_agent_port");
+    if(agent_port != NULL) {
+        fprintf(fp, "agentAddress udp:%s\n\n", agent_port);
+    }
+    else {
+        fprintf(fp, "agentAddress udp:161\n\n");
+    }
+
+    fprintf(fp,"############################################################\n");
+    fprintf(fp,"#\n");
+    fprintf(fp,"#  SYSTEM INFORMATION\n");
+    fprintf(fp,"#\n\n\n");
+
+    const char* sys_description = smap_get(&system_row->other_config, "system_description");
+    if(sys_description != NULL) {
+        fprintf(fp, "sysDescr  %s\n", sys_description);
+    }
+    else {
+        fprintf(fp, "sysDescr  \"\" \n");
+    }
+
+    const char* sys_contact = smap_get(&system_row->other_config, "system_contact");
+    if(sys_contact != NULL) {
+        fprintf(fp, "sysContact  %s\n", sys_contact);
+    }
+    else {
+        fprintf(fp, "sysContact  \"\"\n");
+    }
+
+
+    const char* sys_location = smap_get(&system_row->other_config, "system_location");
+    if(sys_location != NULL) {
+        fprintf(fp, "sysLocation  %s\n", sys_location);
+    }
+    else {
+        fprintf(fp, "sysLocation \"\" \n");
+    }
+
+    fprintf(fp, "sysObjectID    1.3.6.1.4.1.47267\n" );
+
+    fprintf(fp, "sysServices   74 \n");
+
+
+    fprintf(fp,"##############################################################\n");
+    fprintf(fp,"#\n");
+    fprintf(fp,"#  ACCESS CONTROL\n");
+    fprintf(fp,"#\n\n");
+
+    size_t agent_len = system_row->n_snmp_communities;
+    int i = 0;
+    char** temp_snmp_comms = system_row->snmp_communities;
+    if (temp_snmp_comms == NULL) {
+        fprintf(fp, "rocommunity public\n");
+    }
+    else {
+        for (i = 0; i < agent_len; i++) {
+            fprintf(fp, "rocommunity %s\n", *temp_snmp_comms);
+            temp_snmp_comms++;
+        }
+    }
+
+    fprintf(fp,"##############################################################\n");
+    fprintf(fp,"# SNMPv3 AUTHENTICATION and ACCESS CONTROL\n");
+    fprintf(fp,"#\n\n");
+
+    char *auth = NULL;
+    char *priv = NULL;
+
+    if (v3_user) {
+         VLOG_DBG("v3 user exists\n");
+         OVSREC_SNMPV3_USER_FOR_EACH(v3_user , idl) {
+             if (strcmp (v3_user->auth_protocol, "none") != 0) {
+                 if(strcmp(v3_user->auth_protocol,"md5") == 0){
+                     auth = "MD5";
+                 }
+                 else {
+                     auth = "SHA";
+                 }
+                 if (strcmp(v3_user->priv_protocol, "none") != 0){
+                     if (strcmp(v3_user->priv_protocol,"aes") == 0){
+                         priv = "AES";
+                     }
+                     else {
+                         priv = "DES";
+                     }
+                     fprintf(fp, "createUser  %s  %s  %s  %s\n",v3_user->user_name,
+                             auth, v3_user->auth_pass_phrase,
+                             priv );
+                     fprintf(fp, "rouser %s  %s\n",v3_user->user_name, "priv");
+                 }
+                 else {
+                     fprintf(fp, "createUser  %s  %s  %s \n",v3_user->user_name,
+                             auth, v3_user->auth_pass_phrase);
+                     fprintf(fp, "rouser %s\n",v3_user->user_name);
+                 }
+            }
+            else {
+                fprintf(fp, "createUser  %s \n",v3_user->user_name);
+                fprintf(fp, "rouser %s\n",v3_user->user_name);
+            }
+        }
+    }
+
+    fprintf(fp,"##############################################################\n");
+    fprintf(fp,"# SNMPv3 Configuration\n");
+    fprintf(fp,"#\n\n");
+
+    strncpy(snmp_engine_id, "0x0000b8a303", SNMP_ENGINEID_STR_LEN);
+    const char *c = system_row->system_mac;
+    int tmp_idx = 12;
+    while (*c != '\0') {
+        if (*c != ':') {
+            *(snmp_engine_id + tmp_idx) = *c;
+            tmp_idx++;
+        }
+        c++;
+    }
+    *(snmp_engine_id + tmp_idx) = '\0';
+
+    fprintf(fp, "engineID   %s\n",snmp_engine_id);
+    fprintf(fp,"##############################################################\n\n\n");
+
+    fprintf(fp, "master agentx\n");
+    fprintf(fp, "agentXSocket /var/agentx/master\n");
+
+    fclose(fp);
+
+    restart_snmpd();
+    return;
+}
+
+static unsigned int system_seq_no = 0;
+static unsigned int snmpv3_user_seq_no = 0;
+
+static void
+snmp_conf_gen(struct ovsdb_idl* idl){
+    const struct ovsrec_system *system_row = NULL;
+    const struct ovsrec_snmpv3_user *v3_user = NULL;
+
+    system_row = ovsrec_system_first(idl);
+    if(system_row == NULL){
+        VLOG_ERR("Couldn't find system row");
+        return;
+    }
+    v3_user = ovsrec_snmpv3_user_first(idl);
+
+    unsigned int temp_sys_seq_no = ovsrec_system_get_seqno(idl);
+    unsigned int temp_snmpv3_seq_no = ovsrec_snmpv3_user_get_seqno(idl);
+    if((temp_sys_seq_no != system_seq_no) ||
+       (temp_snmpv3_seq_no != snmpv3_user_seq_no)) {
+        system_seq_no = temp_sys_seq_no;
+        snmpv3_user_seq_no = temp_snmpv3_seq_no;
+        generate_snmpd_conf(system_row, v3_user);
+    }
+
+    return;
+}
+
+static void
 snmp_run(struct ovsdb_idl *idl)
 {
     ovsdb_idl_run(idl);
@@ -49,6 +238,8 @@ snmp_run(struct ovsdb_idl *idl)
         VLOG_DBG("checking for system configured");
         return;
     }
+
+    snmp_conf_gen(idl);
 }
 
 static void
@@ -107,6 +298,15 @@ snmpd_ovsdb_init()
 
     ovsdb_idl_add_table(idl, &ovsrec_table_system);
     ovsdb_idl_add_column(idl, &ovsrec_system_col_cur_cfg);
+    ovsdb_idl_add_column(idl, &ovsrec_system_col_snmp_communities);
+    ovsdb_idl_add_column(idl, &ovsrec_system_col_other_config);
+    ovsdb_idl_add_column(idl, &ovsrec_system_col_system_mac);
+    ovsdb_idl_add_table(idl, &ovsrec_table_snmpv3_user);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_user_name);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_auth_protocol);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_auth_pass_phrase);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_priv_protocol);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_priv_pass_phrase);
 
     free(ovsdb_socket);
     VLOG_DBG("OPS snmp OVSDB Integration has been initialized");
