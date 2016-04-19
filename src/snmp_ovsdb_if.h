36071f9ad6308b68c9a93b00e60fa289a0201952
Pooja Kulkarni <pooja.kulkarni@hpe.com>
2016-04-19 05:38:59 -0700

    chg: dev: Added addition support for snmpd.conf generation
    snmp_vty.c : fixed defect 2247
    
    Change-Id: Iecd19f4d18545d7464b0034c73a4a3dd9830c80a
    Signed-off-by: Pooja Kulkarni <pooja.kulkarni@hpe.com>

--- src/snmp_ovsdb_if.h
+++ src/snmp_ovsdb_if.h
@@ -1,6 +1,7 @@
 #ifndef SNMP_OVSDB_IF_H
 #define SNMP_OVSDB_IF_H
 
+#define SNMP_ENGINEID_STR_LEN 32
 /* function declarations */
 void snmpd_ovsdb_init(void);
 void *snmp_ovsdb_main_thread(void *arg);
