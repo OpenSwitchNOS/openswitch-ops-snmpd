a6bf27611ee2841fee1cd299d8e109bf1095a01a
Pooja Kulkarni <pooja.kulkarni@hpe.com>
2016-05-01 23:08:19 -0700

    Fix: dev: Taiga id : 918, 919, 921
    Id 918 : appctl not working for ops-snmpd
    Id 919 : ops-snmpd coring on reboot and on manual systemctl stop ops-snmpd
    Id 212 : System MIB objects not updating on setting through CLI
    
    Change-Id: I7e5327e36fa8d30c3c9032a0718022690d74d520
    Signed-off-by: Pooja Kulkarni <pooja.kulkarni@hpe.com>

--- src/snmp_ovsdb_if.c
+++ src/snmp_ovsdb_if.c
@@ -1,4 +1,5 @@
 #include "dirs.h"
+#include "daemon.h"
 #include "ovsdb-idl.h"
 #include "openvswitch/vlog.h"
 #include "openswitch-idl.h"
@@ -11,10 +12,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include "vswitch-idl.h"
+#include <unixctl.h>
 
 struct ovsdb_idl *idl;
 unsigned int snmp_idl_seqno;
-char *ovsdb_socket;
 bool snmp_exit = false;
 
 VLOG_DEFINE_THIS_MODULE (snmp_ovsdb_if);
@@ -41,6 +42,203 @@ ops_snmpd_system_is_configured(void)
 }/* ops_system_is_configured */
 
 static void
+restart_snmpd()
+{
+    int ret = system("systemctl restart snmpd");
+
+    if(ret < 0)
+    {
+        VLOG_ERR("failed to restart snmpd");
+    }
+
+    return;
+}
+
+static void
+generate_snmpd_conf(const struct ovsrec_system* system_row)
+{
+    char snmp_engine_id[SNMP_ENGINEID_STR_LEN] = {0};
+    FILE* fp = fopen("/etc/snmp/snmpd.conf","w");
+    const struct ovsrec_snmpv3_user *v3_user_row = NULL;
+
+    if(fp == NULL)
+    {
+        VLOG_ERR("Couldn't open snmpd.conf");
+        return;
+    }
+
+    fprintf(fp,"############################################################\n");
+    fprintf(fp,"#\n");
+    fprintf(fp,"#  AGENT BEHAVIOUR\n");
+    fprintf(fp,"#\n\n\n");
+
+    const char* agent_port = smap_get(&system_row->other_config, "snmp_agent_port");
+    if(agent_port != NULL) {
+        fprintf(fp, "agentAddress udp:%s\n\n", agent_port);
+    }
+    else {
+        fprintf(fp, "agentAddress udp:161\n\n");
+    }
+
+    fprintf(fp,"############################################################\n");
+    fprintf(fp,"#\n");
+    fprintf(fp,"#  SYSTEM INFORMATION\n");
+    fprintf(fp,"#\n\n\n");
+
+    const char* sys_description = smap_get(&system_row->other_config, "system_description");
+    if(sys_description != NULL) {
+        fprintf(fp, "sysDescr  %s\n", sys_description);
+    }
+    else {
+        fprintf(fp, "sysDescr  \"\" \n");
+    }
+
+    const char* sys_contact = smap_get(&system_row->other_config, "system_contact");
+    if(sys_contact != NULL) {
+        fprintf(fp, "sysContact  %s\n", sys_contact);
+    }
+    else {
+        fprintf(fp, "sysContact  \"\"\n");
+    }
+
+
+    const char* sys_location = smap_get(&system_row->other_config, "system_location");
+    if(sys_location != NULL) {
+        fprintf(fp, "sysLocation  %s\n", sys_location);
+    }
+    else {
+        fprintf(fp, "sysLocation \"\" \n");
+    }
+
+    fprintf(fp, "sysObjectID    1.3.6.1.4.1.47267\n" );
+
+    fprintf(fp, "sysServices   74 \n");
+
+
+    fprintf(fp,"##############################################################\n");
+    fprintf(fp,"#\n");
+    fprintf(fp,"#  ACCESS CONTROL\n");
+    fprintf(fp,"#\n\n");
+
+    size_t agent_len = system_row->n_snmp_communities;
+    int i = 0;
+    char** temp_snmp_comms = system_row->snmp_communities;
+    if (temp_snmp_comms == NULL) {
+        fprintf(fp, "rocommunity public\n");
+    }
+    else {
+        for (i = 0; i < agent_len; i++) {
+            fprintf(fp, "rocommunity %s\n", *temp_snmp_comms);
+            temp_snmp_comms++;
+        }
+    }
+
+    fprintf(fp,"##############################################################\n");
+    fprintf(fp,"# SNMPv3 AUTHENTICATION and ACCESS CONTROL\n");
+    fprintf(fp,"#\n\n");
+
+    char *auth = NULL;
+    char *priv = NULL;
+
+    OVSREC_SNMPV3_USER_FOR_EACH(v3_user_row , idl) {
+        if (strcmp (v3_user_row->auth_protocol, "none") != 0) {
+             if(strcmp(v3_user_row->auth_protocol,"md5") == 0){
+                 auth = "MD5";
+             }
+             else {
+                 auth = "SHA";
+             }
+             if (strcmp(v3_user_row->priv_protocol, "none") != 0){
+                 if (strcmp(v3_user_row->priv_protocol,"aes") == 0){
+                     priv = "AES";
+                 }
+                 else {
+                     priv = "DES";
+                 }
+                 fprintf(fp, "createUser  %s  %s  %s  %s %s\n",v3_user_row->user_name,
+                         auth, v3_user_row->auth_pass_phrase,
+                         priv, v3_user_row->priv_pass_phrase );
+                 fprintf(fp, "rouser %s  %s\n",v3_user_row->user_name, "priv");
+             }
+             else {
+                 fprintf(fp, "createUser  %s  %s  %s \n",v3_user_row->user_name,
+                         auth, v3_user_row->auth_pass_phrase);
+                 fprintf(fp, "rouser %s %s\n",v3_user_row->user_name, "auth");
+             }
+        }
+        else {
+            fprintf(fp, "createUser  %s \n",v3_user_row->user_name);
+            fprintf(fp, "rouser %s\n",v3_user_row->user_name);
+        }
+    }
+
+    fprintf(fp,"##############################################################\n");
+    fprintf(fp,"# SNMPv3 Configuration\n");
+    fprintf(fp,"#\n\n");
+
+    strncpy(snmp_engine_id, "0x0000b8a303", SNMP_ENGINEID_STR_LEN);
+    const char *c = system_row->system_mac;
+    int tmp_idx = 12;
+    while (*c != '\0') {
+        if (*c != ':') {
+            *(snmp_engine_id + tmp_idx) = *c;
+            tmp_idx++;
+        }
+        c++;
+    }
+    *(snmp_engine_id + tmp_idx) = '\0';
+
+    fprintf(fp, "engineID   %s\n",snmp_engine_id);
+    fprintf(fp,"##############################################################\n\n\n");
+
+    fprintf(fp, "master agentx\n");
+    fprintf(fp, "agentXSocket /var/agentx/master\n");
+
+    fclose(fp);
+
+    restart_snmpd();
+    return;
+}
+
+
+static void
+snmp_reconfigure(struct ovsdb_idl* idl){
+    const struct ovsrec_system *system_row = NULL;
+    const struct ovsrec_snmpv3_user *v3_user_row = NULL;
+    bool modified = false;
+
+    system_row = ovsrec_system_first(idl);
+    if(system_row == NULL){
+        VLOG_ERR("Couldn't find system row");
+        return;
+    }
+
+    v3_user_row = ovsrec_snmpv3_user_first(idl);
+    if(v3_user_row != NULL) {
+        if((OVSREC_IDL_ANY_TABLE_ROWS_MODIFIED(v3_user_row, snmp_idl_seqno)) ||
+           (OVSREC_IDL_ANY_TABLE_ROWS_DELETED(v3_user_row, snmp_idl_seqno))  ||
+           (OVSREC_IDL_ANY_TABLE_ROWS_INSERTED(v3_user_row, snmp_idl_seqno)) ) {
+            modified = true ;
+        }
+    }
+
+    if(OVSREC_IDL_IS_COLUMN_MODIFIED(ovsrec_system_col_snmp_communities,
+                                       snmp_idl_seqno)) {
+        modified = true;
+    }
+    if(OVSREC_IDL_IS_COLUMN_MODIFIED(ovsrec_system_col_other_config,
+                                       snmp_idl_seqno)) {
+        modified = true;
+    }
+
+    if(modified) {
+        generate_snmpd_conf(system_row);
+    }
+
+    return;
+}
+
+static void
 snmp_run(struct ovsdb_idl *idl)
 {
     ovsdb_idl_run(idl);
@@ -48,6 +246,13 @@ snmp_run(struct ovsdb_idl *idl)
     if(!ops_snmpd_system_is_configured()) {
         VLOG_DBG("checking for system configured");
         return;
+    }
+
+    unsigned int temp_seq_no = ovsdb_idl_get_seqno(idl);
+
+    if (temp_seq_no != snmp_idl_seqno) {
+        snmp_reconfigure(idl);
+        snmp_idl_seqno = ovsdb_idl_get_seqno(idl);
     }
 }
 
@@ -57,12 +262,15 @@ snmp_wait(struct ovsdb_idl *idl)
    ovsdb_idl_wait (idl);
 }
 
-
 void *
 snmp_ovsdb_main_thread(void *arg)
 {
+    struct unixctl_server *appctl;
+
     /* Detach thread to avoid memory leak upon exit. */
     pthread_detach(pthread_self());
+
+    appctl = (struct unixctl_server *)arg;
 
     snmp_exit = false;
     while (!snmp_exit) {
@@ -71,34 +279,41 @@ snmp_ovsdb_main_thread(void *arg)
         /* This function updates the Cache by running
            ovsdb_idl_run. */
         snmp_run(idl);
+        unixctl_server_run(appctl);
 
         /* This function adds the file descriptor for the
            DB to monitor using poll_fd_wait. */
         snmp_wait(idl);
+        unixctl_server_wait(appctl);
 
         SNMP_OVSDB_UNLOCK;
         if (snmp_exit) {
             poll_immediate_wake();
         } else {
             poll_block();
+            if (should_service_stop()) {
+                snmp_exit = true;
+            }
         }
     }
     VLOG_ERR("Exiting OVSDB main thread");
+    unixctl_server_destroy(appctl);
     return NULL;
 }
 
 void
-snmpd_ovsdb_init()
+snmpd_ovsdb_init(const char *snmp_db_path)
 {
     long int pid;
     char *idl_lock;
 
-    ovsdb_socket = xasprintf("unix:%s/db.sock", ovs_rundir());
     ovsrec_init();
 
-    idl = ovsdb_idl_create(ovsdb_socket, &ovsrec_idl_class, false , true);
+    idl = ovsdb_idl_create(snmp_db_path, &ovsrec_idl_class, false , true);
+
     pid = getpid();
     idl_lock = xasprintf("ops_snmp_%ld", pid);
+
     ovsdb_idl_set_lock(idl, idl_lock);
     free(idl_lock);
 
@@ -107,8 +322,16 @@ snmpd_ovsdb_init()
 
     ovsdb_idl_add_table(idl, &ovsrec_table_system);
     ovsdb_idl_add_column(idl, &ovsrec_system_col_cur_cfg);
+    ovsdb_idl_add_column(idl, &ovsrec_system_col_snmp_communities);
+    ovsdb_idl_add_column(idl, &ovsrec_system_col_other_config);
+    ovsdb_idl_add_column(idl, &ovsrec_system_col_system_mac);
+    ovsdb_idl_add_table(idl, &ovsrec_table_snmpv3_user);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_user_name);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_auth_protocol);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_auth_pass_phrase);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_priv_protocol);
+    ovsdb_idl_add_column(idl, &ovsrec_snmpv3_user_col_priv_pass_phrase);
 
-    free(ovsdb_socket);
     VLOG_DBG("OPS snmp OVSDB Integration has been initialized");
     return;
 }
--- src/snmp_plugins.c
+++ src/snmp_plugins.c
@@ -23,16 +23,107 @@
  *          via plugins.
  */
 
+
 #include <config.h>
 #include <errno.h>
 #include <ltdl.h>
 #include <unistd.h>
 #include "snmp_plugins.h"
+#include "coverage.h"
+#include "dynamic-string.h"
 #include "openvswitch/vlog.h"
 
-#define SNMP_PLUGINS_ERR -1
-
 VLOG_DEFINE_THIS_MODULE(snmp_plugins);
+
+typedef void(*plugin_func)(void);
+
+struct plugin_class {
+    plugin_func ops_snmp_init;
+    plugin_func ops_snmp_run;
+    plugin_func ops_snmp_wait;
+    plugin_func ops_snmp_destroy;
+};
+
+static lt_dlinterface_id interface_id;
+
+static int
+plugins_open_plugin(const char *filename, void *data)
+{
+    struct plugin_class *plcl;
+    lt_dlhandle handle;
+
+    if (!(handle = lt_dlopenadvise(filename, *(lt_dladvise *)data))) {
+        VLOG_ERR("Failed loading %s: %s", filename, lt_dlerror());
+        return 0;
+    }
+
+    if (!(plcl = (struct plugin_class *)malloc(sizeof(struct plugin_class)))) {
+        VLOG_ERR("Couldn't allocate plugin class");
+        goto err_plugin_class;
+    }
+
+    if (!(plcl->ops_snmp_init = lt_dlsym(handle, "ops_snmp_init")) ||
+        !(plcl->ops_snmp_destroy = lt_dlsym(handle, "ops_snmp_destroy"))) {
+            VLOG_ERR("Couldn't initialize the interface for %s", filename);
+            goto err_dlsym;
+    }
+
+
+    plcl->ops_snmp_init();
+
+    VLOG_INFO("Loaded SNMP plugin library %s", filename);
+    return 0;
+
+err_dlsym:
+    free(plcl);
+
+err_plugin_class:
+    if (lt_dlclose(handle)) {
+        VLOG_ERR("Couldn't dlclose %s", filename);
+    }
+
+    return 0;
+}
+
+void
+plugins_snmp_init(const char *path)
+{
+    lt_dladvise advise;
+
+    if (path && !strcmp(path, "none")) {
+        return;
+    }
+
+    if (lt_dlinit() ||
+        lt_dlsetsearchpath(path) ||
+        lt_dladvise_init(&advise)) {
+        VLOG_ERR("ltdl initializations: %s", lt_dlerror());
+    }
+
+    if (!(interface_id = lt_dlinterface_register("ops-snmpd", NULL))) {
+        VLOG_ERR("lt_dlinterface_register: %s", lt_dlerror());
+        goto err_interface_register;
+    }
+
+    if (lt_dladvise_global(&advise) || lt_dladvise_ext (&advise) ||
+        lt_dlforeachfile(lt_dlgetsearchpath(), &plugins_open_plugin, &advise)) {
+        VLOG_ERR("ltdl setting advise: %s", lt_dlerror());
+        goto err_set_advise;
+    }
+
+    VLOG_INFO("Successfully initialized all SNMP plugins");
+    return;
+
+err_set_advise:
+    lt_dlinterface_free(interface_id);
+
+err_interface_register:
+    if (lt_dladvise_destroy(&advise)) {
+        VLOG_ERR("destroying ltdl advise%s", lt_dlerror());
+        return;
+    }
+
+}
 
 #define PLUGINS_CALL(FUNC) \
 do { \
@@ -46,29 +137,18 @@ do { \
     } \
 }while(0)
 
-typedef void(*plugin_func)(void);
+void
+plugins_snmp_run(void)
+{
+    PLUGINS_CALL(ops_snmp_run);
+}
 
-/* plugin_class structure used for calling feature specific
- * snmp init function by plugin_func.
- */
-struct plugin_class {
-    plugin_func    ops_snmp_init;
-    plugin_func    ops_snmp_run;
-    plugin_func    ops_snmp_wait;
-    plugin_func    ops_snmp_destroy;
-};
+void
+plugins_snmp_wait(void)
+{
+    PLUGINS_CALL(ops_snmp_wait);
+}
 
-/*
- * Unique key to store and retrieve per-module data.
- */
-static lt_dlinterface_id interface_id;
-
-/*
- * Function : plugins_snmp_destroy.
- * Responsibility : unloading all feature specific snmp module.
- * Parameters : void.
- * Return : void.
- */
 void
 plugins_snmp_destroy(void)
 {
@@ -76,126 +156,4 @@ plugins_snmp_destroy(void)
     lt_dlinterface_free(interface_id);
     lt_dlexit();
     VLOG_INFO("Destroyed all plugins");
-    exit(1);
-}
-
-void
-plugins_snmp_run(void)
-{
-    struct plugin_class plcl = {NULL};
-    plcl.ops_snmp_run();
-}
-
-void
-plugins_snmp_wait(void)
-{
-    struct plugin_class plcl = {NULL};
-    plcl.ops_snmp_wait();
-}
-
-
-
-/*
- * Function : plugins_open_plugin.
- * Responsibility : load and call the feature specific module and init function.
- * Parameters :
- *   const char *filename: module filename which is passed by libltdl.
- *   void *data : contains module loading modes which is initialized by libltdl.
- * Return : return 0 on suceess.
- */
-static int
-plugins_open_plugin(const char *filename, void *data)
-{
-    struct plugin_class plcl = {NULL};
-    lt_dlhandle handle;
-
-    if (!(handle = lt_dlopenadvise(filename, *(lt_dladvise *)data))) {
-        VLOG_ERR("Failed loading %s: %s\n", filename, lt_dlerror());
-        plugins_snmp_destroy();
-        return SNMP_PLUGINS_ERR;
-    }
-
-    plcl.ops_snmp_init = lt_dlsym(handle, "ops_snmp_init");
-    if (plcl.ops_snmp_init == NULL) {
-        plugins_snmp_destroy();
-        return SNMP_PLUGINS_ERR;
-    }
-
-    plcl.ops_snmp_run = lt_dlsym(handle, "ops_snmp_run");
-    if (plcl.ops_snmp_run == NULL) {
-        plugins_snmp_destroy();
-        return SNMP_PLUGINS_ERR;
-    }
-    plcl.ops_snmp_wait = lt_dlsym(handle, "ops_snmp_wait");
-    if (plcl.ops_snmp_wait == NULL) {
-        plugins_snmp_destroy();
-        return SNMP_PLUGINS_ERR;
-    }
-    plcl.ops_snmp_destroy = lt_dlsym(handle, "ops_snmp_destroy");
-    if (plcl.ops_snmp_destroy == NULL) {
-        plugins_snmp_destroy();
-        return SNMP_PLUGINS_ERR;
-    }
-
-    if (lt_dlcaller_set_data(interface_id, handle, &plcl)) {
-        VLOG_ERR("plugin %s initialized twice\n", filename);
-        plugins_snmp_destroy();
-        return SNMP_PLUGINS_ERR;
-    }
-
-    plcl.ops_snmp_init();
-
-    VLOG_DBG("Loaded SNMP plugin library %s\n", filename);
-    return 0;
-}
-
-/*
- * Function : plugins_snmp_init.
- * Responsibility : Initialize feature specific snmp.
- * Parameters : const char *path : libltdl search path.
- * Return : void.
- */
-void
-plugins_snmp_init(const char *path)
-{
-     lt_dladvise advise;
-
-    /* Initialize libltdl, set the libltdl serach path,
-     * initialize advise parameter,which is used pass hints
-     * to module loader when using lt_dlopenadvise to perform the loading.
-     plugins_snmp_init*/
-    if (lt_dlinit() ||
-        lt_dlsetsearchpath(path) ||
-        lt_dladvise_init(&advise)) {
-        VLOG_ERR("ltdl initializations: %s\n", lt_dlerror());
-        return;
-    }
-
-    /* Register ops-snmpd interface validator with libltdl */
-    /* TO-DO : repo name will change to ops-snmpd */
-    if (!(interface_id = lt_dlinterface_register("ops-snmpd", NULL))) {
-        VLOG_ERR("lt_dlinterface_register: %s\n", lt_dlerror());
-        if (lt_dladvise_destroy(&advise)) {
-            VLOG_ERR("destroying ltdl advise%s\n", lt_dlerror());
-        }
-        return;
-    }
-
-    /* set symglobal hint and call the feature specific snmp init function via
-     * plugins_open_plugin function pointer.
-     * 'lt_dlforeachfile' function will continue to make calls to
-     * 'plugins_open_plugin()' for each file that it discovers in search_path
-     * until one of these calls returns non-zero, or until the
-     * files are exhausted.
-     * `lt_dlforeachfile' returns value returned by the last call
-     * made to 'plugins_open_plugin'.
-     */
-    if (lt_dladvise_global(&advise) || lt_dladvise_ext (&advise) ||
-        lt_dlforeachfile(lt_dlgetsearchpath(), &plugins_open_plugin, &advise)) {
-        VLOG_ERR("ltdl setting advise: %s\n", lt_dlerror());
-        return;
-    }
-
-    VLOG_INFO("Successfully initialized all snmp plugins");
-    return;
 }
